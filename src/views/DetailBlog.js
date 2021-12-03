f688883394fa77ee9c3edd664052c3795cfd4c20
se7en <se7en.ntt@gmail.com>
2021-12-03 21:33:48 +0700

    controlled uncontrolled

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,10 +1,39 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
+import useFetch from '../customize/fetch';
+import './Blog.scss';
 
 const DetailBlog = () => {
     let { id } = useParams();
-    console.log('>>> check use param: ', useParams())
+    let history = useHistory();
+
+
+
+    const { data: dataBlogDetail, isLoading, isError }
+        = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`, false)
+
+    const handleBackData = () => {
+        history.push("/blog");
+    }
+
+
     return (
-        <h1> Hello detail blogs with id = {id}</h1>
+        <>
+            <div> <span onClick={handleBackData}>&lt;-- Back </span></div>
+            <div className="blog-detail">
+                {dataBlogDetail &&
+                    <>
+                        <div className="title">
+                            Blog ID: {id} ---  {isLoading === true ? 'Loading data ...' : dataBlogDetail.title}
+                        </div>
+                        <div className="content">
+                            {dataBlogDetail.body}
+                        </div>
+
+                    </>
+                }
+
+            </div>
+        </>
     )
 }
 
