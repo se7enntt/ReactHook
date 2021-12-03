67c1d9725ef1bc971c3675c72407193a8588b8a2
se7en <se7en.ntt@gmail.com>
2021-12-03 22:26:12 +0700

    video29 NotFound

--- src/views/AddNewBlog.js
+++ src/views/AddNewBlog.js
@@ -1,13 +1,37 @@
 import './Blog.scss';
 import { useState } from 'react';
+import axios from 'axios';
 
-const AddNewBlog = () => {
+const AddNewBlog = (props) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
 
-    const handleSubmitBtn = () => {
-        console.log('>>>> check data state: ', title, content)
+    const handleSubmitBtn = async () => {
+        if (!title) {
+            alert('empty title');
+            return;
+        }
+        if (!content) {
+            alert('empty content')
+            return;
+        }
+
+        let data = {
+            title: title,
+            body: content,
+            userId: 1,
+        }
+
+        let res = await axios.post('https://jsonplaceholder.typicode.com/posts', data);
+        if (res && res.data) {
+            let newBlog = res.data;
+            props.handleAddNew(newBlog);
+        }
+
     }
+
+
+
     return (
         <div className="add-new-container">
 
